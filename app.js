bf0cee9425707a7b480cc21062671ce47168b883
JQiue <861947542@qq.com>
2021-01-07 11:59:34 +0800

    init

---
+++ app.js
@@ -0,0 +1,36 @@
+const https = require('https');
+const http = require('http');
+
+http.createServer((req, res) => {
+  res.end("OK!!!")
+}).listen(3000)
+
+function wake(url) {
+  https.get(url, res => {
+    if (res.statusCode == 200) {
+      console.log(url + "=> 唤醒成功，StatusCode：" + res.statusCode);
+    } else {
+      console.log(url + "=> 唤醒失败");
+    }
+  })
+}
+
+const callUrl = "https://jqiue.avosapps.us/"
+const wake1 = "https://wake1.avosapps.us/"
+const wake2 = "https://wake2.avosapps.us/"
+
+setInterval(() => {
+  let hour = (new Date().getHours() + 8) % 25;
+  console.log("current time：" + hour);
+  if (hour >= 12 && hour <= 24) {
+    wake(wake1)
+    if (hour <= 1 || hour >= 8) {
+      wake(callUrl)
+    }
+  } else {
+    wake(wake2)
+    if (hour <= 1 || hour >= 8) {
+      wake(callUrl)
+    }
+  }
+}, 1650000);
