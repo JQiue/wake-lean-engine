0da44e9de7cfdd00fb6cc3fc8e85eb1a7663ba55
JQiue <861947542@qq.com>
2021-03-31 17:42:59 +0800

    feat: 能唤醒更多服务器，根据环境变量的配置工作

--- app.js
+++ app.js
@@ -1,36 +1,61 @@
 const https = require('https');
 const http = require('http');
 
-http.createServer((req, res) => {
-  res.end("OK!!!")
-}).listen(3000)
+const wakeInterval = Number(process.env.INTERVAL_TIME) || 1500000;
+const hour = (new Date().getHours() + 8) % 24;
+
+console.log('间隔唤醒时间：' + wakeInterval);
 
 function wake(url) {
   https.get(url, res => {
-    if (res.statusCode == 200) {
-      console.log(url + "=> 唤醒成功，StatusCode：" + res.statusCode);
-    } else {
-      console.log(url + "=> 唤醒失败，StatusCode" + res.statusCode);
-    }
+    res.statusCode == 200 ? console.log(url + "=> 唤醒成功，StatusCode：" + res.statusCode) : console.log(url + "=> 唤醒失败，StatusCode：" + res.statusCode)
   })
 }
 
-const callUrl = "https://jqiue.avosapps.us/"
-const wake1 = "https://wake1.avosapps.us/"
-const wake2 = "https://wake2.avosapps.us/"
+if (process.env.NODE_ENV) {
+  console.log('development environment');
+  const callUrl = "https://jqiue.avosapps.us/";
+  const wake1 = "https://wake1.avosapps.us/";
+  const wake2 = "https://wake2.avosapps.us/";
+  setInterval(() => {
+    console.log("current hour：" + hour);
+    if (hour >= 12 && hour <= 23) {
+      wake(wake1);
+      if (hour <= 1 || hour >= 8) {
+        wake(callUrl);
+      }
+    } else {
+      wake(wake2);
+      if (hour <= 1 || hour >= 8) {
+        wake(callUrl);
+      }
+    }
+  }, wakeInterval);
+} else {
+  console.log('production environment');
+  const url_list = process.env.CALL_URLS.split(';');
+  const wake1 = process.env.WAKE1;
+  const wake2 = process.env.WAKE2;
+  setInterval(() => {
+    console.log("current hour：" + hour);
+    if (hour >= 12 && hour <= 23) {
+      wake(wake1);
+      if (hour <= 1 || hour >= 8) {
+        url_list.forEach(url => {
+          wake(url);
+        });
+      }
+    } else {
+      wake(wake2);
+      if (hour <= 1 || hour >= 8) {
+        url_list.forEach(url => {
+          wake(url);
+        });
+      }
+    }
+  }, wakeInterval);
+}
 
-setInterval(() => {
-  let hour = (new Date().getHours() + 8) % 24;
-  console.log("current time：" + hour);
-  if (hour >= 12 && hour <= 23) {
-    wake(wake1)
-    if (hour <= 1 || hour >= 8) {
-      wake(callUrl)
-    }
-  } else {
-    wake(wake2)
-    if (hour <= 1 || hour >= 8) {
-      wake(callUrl)
-    }
-  }
-}, 1650000);
+http.createServer((req, res) => {
+  res.end("OK!!!")
+}).listen(3000);
--- push.sh
+++ push.sh
@@ -1,3 +1,3 @@
 git add .
-git commit -m "fix: 修复唤醒时间误差问题"
+git commit -m "feat: 能唤醒更多服务器，根据环境变量的配置工作"
 git push origin master
